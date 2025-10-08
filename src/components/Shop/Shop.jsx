063fda0a2677de1811eef4370cd9924029f240dc
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-10-08 12:18:59 +0600

    load data based on the page number and size

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -22,10 +22,10 @@ const Shop = () => {
     console.log(pages)
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?pages=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
